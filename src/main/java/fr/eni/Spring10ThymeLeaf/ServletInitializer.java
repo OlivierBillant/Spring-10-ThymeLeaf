2342dfd5a1a7da7a624229830121a52c3280f2ca
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-07-27 13:32:39 +0200

    CREATE

---
+++ src/main/java/fr/eni/Spring10ThymeLeaf/ServletInitializer.java
@@ -0,0 +1,13 @@
+package fr.eni.Spring10ThymeLeaf;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Spring10ThymeLeafApplication.class);
+	}
+
+}
---
+++ src/main/java/fr/eni/Spring10ThymeLeaf/Spring10ThymeLeafApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.Spring10ThymeLeaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring10ThymeLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring10ThymeLeafApplication.class, args);
+	}
+
+}
